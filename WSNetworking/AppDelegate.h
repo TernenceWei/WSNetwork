fd7f91e9db12fd7b3e4cf64de0a0504da094689c
Ternence <13297934882@163.com>
2018-04-28 17:36:59 +0800

    first commit add afn

---
+++ WSNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSNetworking
+//
+//  Created by 魏山 on 2018/4/28.
+//  Copyright © 2018年 yyhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSNetworking
+//
+//  Created by 魏山 on 2018/4/28.
+//  Copyright © 2018年 yyhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
